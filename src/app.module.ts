f7599b2a038e8fe331bc1b86402dee1d505002d2
Huy Le <huylhddev98@gmail.com>
2022-10-13 19:58:11 +0700

    setup postgres database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { appConfig } from "./config";
+import DatabaseModule from "./database/database.module";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [appConfig] }),
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/index.ts
@@ -0,0 +1,13 @@
+export const appConfig = () => ({
+  database: {
+    type: "postgres",
+    host: process.env.DB_HOST || "localhost",
+    port: parseInt(process.env.DB_PORT, 10) || 5432,
+    database: process.env.DB_NAME || "test",
+    user: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASSWORD || "123456",
+    entities: ["dist/**/*.entity.js"],
+    synchronize: true,
+    logging: ["query"],
+  },
+});
